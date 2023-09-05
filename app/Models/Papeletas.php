8530a9994bf4030c43706dd6dd13d2758433c5ff
Jhonatan Mantilla <jhonatanmm.1995@gmail.com>
2023-09-05 07:16:36 -0500

    modificando modelos

--- app/Models/Papeletas.php
+++ app/Models/Papeletas.php
@@ -40,4 +40,31 @@ class Papeletas
       return 0;
     }
   }
+
+
+  public function consultarPapeletasPorPlaca($placa)
+  {
+    $sql = "SELECT DISTINCT CONCAT(P.PER_apellidos, ', ', P.PER_nombre) AS Conductor, A.ASO_razonsocial, A.ASO_direccion, A.ASO_colores 
+              FROM UNIDAD U
+              LEFT JOIN ASOCIACION A ON U.UNI_codigo = A.ASO_codigo
+              INNER JOIN PERSONA P ON P.PER_codigo = U.PRO_codigo
+              WHERE U.UNI_placa = :placa";
+
+    try {
+      $stmt = $this->conector->prepare($sql);
+      $stmt->bindParam(":placa", $placa, PDO::PARAM_STR);
+      $stmt->execute();
+
+      // Utiliza fetchAll para obtener todos los resultados de una vez
+      $resultados = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+      // Cierra la conexión para liberar recursos
+      $stmt->closeCursor();
+
+      return $resultados;
+    } catch (PDOException $e) {
+      // Puedes manejar el error aquí (por ejemplo, log de errores)
+      return false;
+    }
+  }
 }
--- app/Models/Paraderos.php
+++ app/Models/Paraderos.php
@@ -1,5 +1,4 @@
 <?php
-// Incluye el archivo de configuración de la base de datos
 require_once 'config/conexion.php';
 
 class Paradero
@@ -8,10 +7,32 @@ class Paradero
 
     public function __construct()
     {
-        $this->conector = (new Conexion())->getConexion(); // Obtén la conexión a la base de datos directamente
+        $this->conector = (new Conexion())->getConexion();
     }
-    
-    public function consultarParadero(){
-        
+
+    public function consultarParaderoPorPlaca($placa)
+    {
+        $sql = "SELECT DISTINCT CONCAT(P.PER_apellidos, ', ', P.PER_nombre) AS Conductor, A.ASO_razonsocial, A.ASO_direccion, A.ASO_colores 
+                FROM UNIDAD U
+                LEFT JOIN ASOCIACION A ON U.UNI_codigo = A.ASO_codigo
+                INNER JOIN PERSONA P ON P.PER_codigo = U.PRO_codigo
+                WHERE U.UNI_placa = :placa";
+
+        try {
+            $stmt = $this->conector->prepare($sql);
+            $stmt->bindParam(":placa", $placa, PDO::PARAM_STR);
+            $stmt->execute();
+
+            // Utiliza fetchAll para obtener todos los resultados de una vez
+            $resultados = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+            // Cierra la conexión para liberar recursos
+            $stmt->closeCursor();
+
+            return $resultados;
+        } catch (PDOException $e) {
+            // Puedes manejar el error aquí (por ejemplo, log de errores)
+            return false;
+        }
     }
 }
