e4d3fd68be3db38ba5d3b2c9cbc4ca7a97fec082
gamm95 <jhonatanmm.1995@gmail.com>
2023-10-02 10:13:29 -0500

    Arreglo de papeletas

--- app/Views/PnlPapeletas.php
+++ app/Views/PnlPapeletas.php
@@ -71,7 +71,7 @@
                                 <tbody>
                                     <?php foreach ($resultadoBusqueda as $papeleta) : ?>
                                         <tr class=" bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-                                            <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                            <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-normal overflow-ellipsis dark:text-white">
                                                 <?php echo $papeleta['propietario']; ?>
                                             </th>
                                             <td class="px-6 py-4 dark:text-white">
